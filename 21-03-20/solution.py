a8c18dab0abe243f0d8f872c769a25804d040e0d
James Cockbain <james.cockbain@ibm.com>
2020-03-21 18:04:15 +0000

    add 2nd solution for 21-03-20

--- 21-03-20/solution.py
+++ 21-03-20/solution.py
@@ -1,11 +1,25 @@
 import unittest
 
+# O(N^2)
 
-def is_sum_1(arr, target):
+
+def can_make_target1(arr, target):
     for i in range(len(arr)):
         for j in range(i, len(arr)):
             if arr[i] + arr[j] == target:
                 return True
+    return False
+
+# O(N)
+
+
+def can_make_target2(arr, target):
+    looking_for = set()
+    for a in arr:
+        if a in looking_for:
+            return True
+        else:
+            looking_for.append(target - a)
     return False
 
 
@@ -13,10 +27,16 @@ class test(unittest.TestCase):
     def test_find_subsets(self):
         arr = [1, 2, 3, 4]
         arr2 = [2, 4, 6, 10]
-        self.assertEqual(is_sum_1(arr, 4), True)
-        self.assertEqual(is_sum_1(arr, 12), False)
-        self.assertEqual(is_sum_1(arr2, 12), True)
-        self.assertEqual(is_sum_1(arr2, 7), False)
+
+        self.assertEqual(can_make_target1(arr, 4), True)
+        self.assertEqual(can_make_target1(arr, 12), False)
+        self.assertEqual(can_make_target1(arr2, 12), True)
+        self.assertEqual(can_make_target1(arr2, 7), False)
+
+        self.assertEqual(can_make_target2(arr, 4), True)
+        self.assertEqual(can_make_target2(arr, 12), False)
+        self.assertEqual(can_make_target2(arr2, 12), True)
+        self.assertEqual(can_make_target2(arr2, 7), False)
 
 
 if __name__ == "__main__":

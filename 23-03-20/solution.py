edf9ac9b7bbbb15004b8171d9cc7be7aa40e615b
James Cockbain <james.cockbain@ibm.com>
2020-03-23 20:51:53 +0000

    add solution for 23-03-20

--- 23-03-20/solution.py
+++ 23-03-20/solution.py
@@ -1,27 +1,50 @@
 import unittest
 
+
 class Node:
     def __init__(self, val, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 def serialize(node):
-    pass
+    vals = []
+
+    def encode(node):
+        if node:
+            vals.append(str(node.val))
+            encode(node.left)
+            encode(node.right)
+        else:
+            vals.append('#')
+    encode(node)
+    return ' '.join(vals)
 
 
-def deserialize(node):
-    pass
+def deserialize(vals):
+    def decode(vals):
+        val = next(vals)
+        if val == "#":
+            return None
+        node = Node(val)
+        node.left = decode(vals)
+        node.right = decode(vals)
+        return node
 
+    vals = iter(vals.split())
+    return decode(vals)
 
 
 class test(unittest.TestCase):
     def test_serialize(self):
-
-
         node = Node('root', Node('left', Node('left.left')), Node('right'))
+        serialized = 'root left left.left # # # right # #'
+        self.assertEqual(serialized, serialize(node))
+
         res = deserialize(serialize(node)).left.left.val
         self.assertEqual('left.left', res)
+
 
 if __name__ == "__main__":
     unittest.main()

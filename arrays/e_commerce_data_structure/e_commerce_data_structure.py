baaad08ced20da606cd8ec446ae81ee554a6a03e
James Cockbain <james.cockbain@ibm.com>
2020-04-06 21:03:36 +0100

    add second solution for 05-04-20

--- arrays/e_commerce_data_structure/e_commerce_data_structure.py
+++ arrays/e_commerce_data_structure/e_commerce_data_structure.py
@@ -1,9 +1,29 @@
 class order_array():
-    def __init__(self):
+    def __init__(self, n):
         self.orders = []
+        self.n = n
 
     def record(self, order_id):
+        if len(self.orders) >= self.n:
+            self.orders.pop(0)
         self.orders.append(order_id)
 
     def get_last(self, i):
         return self.orders[-i]
+
+
+class order_circular_array(object):
+    def __init__(self, n):
+        self.n = n
+        self.orders = []
+        self.cur = 0
+
+    def record(self, order_id):
+        if len(self.orders) == self.n:
+            self.orders[self.cur] = order_id
+        else:
+            self.orders.append(order_id)
+        self.cur = (self.cur + 1) % self.n
+
+    def get_last(self, i):
+        return self.orders[self.cur - i]
--- arrays/e_commerce_data_structure/test_e_commerce_data_structure.py
+++ arrays/e_commerce_data_structure/test_e_commerce_data_structure.py
@@ -1,11 +1,17 @@
 import unittest
 
-from e_commerce_data_structure import order_array
+from e_commerce_data_structure import order_array, order_circular_array
 
 
 class test(unittest.TestCase):
     def test_order_array(self):
-        orders = order_array()
+        orders = order_array(5)
+        orders.record(1)
+        orders.record(2)
+        self.assertEqual(1, orders.get_last(2))
+
+    def test_order_circular_array(self):
+        orders = order_circular_array(5)
         orders.record(1)
         orders.record(2)
         self.assertEqual(1, orders.get_last(2))
